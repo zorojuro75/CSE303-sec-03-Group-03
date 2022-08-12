eecdff6123e5d9d36dbe45ef61e458cb6432071b
S. M. A Sayed Al Banna <2021591@iub.edu.bd>
2022-08-12 23:05:57 +0600

    created index.html

--- AQI/settings.py
+++ AQI/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authentication',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'AQI.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["template"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,6 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def home(request):
+    return render(request, "authentication/index.html")
