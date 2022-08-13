546e9d04ad82cb7594cf8d9c068c37044e4df950
S. M. A Sayed Al Banna <2021591@iub.edu.bd>
2022-08-13 22:31:15 +0600

    authentication

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,17 @@
 from http.client import HTTPResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
 # Create your views here.
 def home(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return render(request, "dashboard/index.html")
+        else:
+            return render(request, "dashboard/index.html")
     return render(request, "authentication/index.html")
