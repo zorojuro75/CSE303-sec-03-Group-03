f0a3b40773e176da4114cde74c28897f51d0d2de
S. M. A Sayed Al Banna <2021591@iub.edu.bd>
2022-08-14 21:40:52 +0600

    mysql

--- AQI/settings.py
+++ AQI/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'AQI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'auth',
+        'USER': 'root',
+        'PASSWORD': 'poralekhakoreultayfelsi',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
