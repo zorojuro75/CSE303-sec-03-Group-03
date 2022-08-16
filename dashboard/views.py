99ca90c2174423ce721175ee4ff8aca2b1fe53d4
S. M. A Sayed Al Banna <2021591@iub.edu.bd>
2022-08-16 23:37:21 +0600

    delete all

--- dashboard/views.py
+++
@@ -1,8 +0,0 @@
-from multiprocessing import context
-from django.shortcuts import render
-from dashboard.models import MemberT
-def home(request):
-    if request.method == "POST":
-        hello = "hello" 
-        return render(request, 'dashboard/index.html', hello)
-    return render(request, 'dashboard/index.html', hello)
